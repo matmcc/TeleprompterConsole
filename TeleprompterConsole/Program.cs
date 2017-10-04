d609057399810aa2257708dc908196d3c441151e
matmcc <matmcconkey@gmail.com>
2017-10-04 20:33:40 +0100

    word by word print to console, 200ms delay

---
+++ TeleprompterConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeleprompterConsole
+{
+    class Program
+    {
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // yield return line;
+                    var words = line.Split(' ');
+                    foreach (var word in words)
+                    { yield return word + " "; }
+                    yield return Environment.NewLine;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var line in lines)
+            {
+                // Console.WriteLine(line);
+                Console.Write(line);
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    var pause = Task.Delay(200);
+                    pause.Wait();
+                }
+            }
+        }
+    }
+}
